c6b79088252966e6dbd012f278734fb07d9084a2
Caue Hironimuz Marcondes <cmarcondes@daitangroup.com>
2017-02-10 11:25:37 -0200

    initial commit react

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 5000
+  }
+};
